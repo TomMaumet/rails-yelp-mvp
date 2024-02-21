eb8792643b0e9b8680d14aeeabd3abf7d3eaa94c
Thomas Maumet <thomasmaumet@free.fr>
2024-02-21 16:43:29 +0100

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'french' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+my_sushi = { name: 'My Sushi', address: 'Quai des Marques, Bordeaux', category: 'japanese' }
+moule_frite = { name: 'La Moule et la Frite', address: 'Rue du Palais Bourbon, Paris', category: 'belgian' }
+china_delices = { name: 'Les Délices de Chine', address: 'Quai de Paludate, Bordeaux', category: 'chinese' }
+
+[dishoom, pizza_east, my_sushi, moule_frite, china_delices].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
